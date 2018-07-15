23a5935a83914fc04c47379dd90e99465b992cb1
vitorkei <vitor_kei9@hotmail.com>
2018-07-15 18:04:56 -0300

    update_pos() do Asteroids.py deve conseguir procurar e acompanhar a movimentação dos asteróides corretamente, mas ainda não detecta os "asteróides filhos" que surgem quando um grande ou médio é destruído

--- gym-retro/examples/Asteroids.py
+++ gym-retro/examples/Asteroids.py
@@ -2,6 +2,7 @@ import posMethods as pM
 import speedMethods as sM
 import auxMethods as aM
 import numpy as np
+import time
 
 SCREEN_UPPER_LIMIT = 18
 SCREEN_LOWER_LIMIT = 195
@@ -17,10 +18,10 @@ class Asteroids:
 
     self.count = len(iniPos)
     self.pos = dict()
-    self.spd = np.array((0, 0))
 
     for i in range(self.count):
       self.pos[i] = iniPos[i]
+      self.pos[i]['spd'] = [0, 0] # (h_vel, v_vel)
 
   def get_asteroids(self):
     return self.pos
@@ -49,18 +50,20 @@ class Asteroids:
     return upB, loB, leB, riB
   
   def get_smaller_asteroids(self, obs, old_asteroid):
+    upB, loB, leB, riB = self.get_search_bounds(old_asteroid, 5)
+
     pass 
 
   # Atualiza a posição dos asteroides
   # Precisa ser chamada em todos os frames
   # que os asteroides aparecem (frames pares)
-  def update_pos(self, obs):
+  def update_pos(self, obs, delta):
     destroyed_asteroids = []
     for ID, elem in self.pos.items():
       color = elem['color']
     
       # Determinação dos novos limites. Vide documentação
-      upB, loB, leB, riB = self.get_search_bounds(elem, 2)
+      upB, loB, leB, riB = self.get_search_bounds(elem, delta)
 
       asteroid_destroyed = True
       # alteração VERTICAL/HORIZONTAL
@@ -71,10 +74,11 @@ class Asteroids:
           if np.array_equal(obs[i][j], color):
 
             diff = i - elem['upperBound']
+            elem['spd'][1] = diff
             #print("v_diff =", diff)
 
             elem['upperBound'] = i
-            elem['lowerBound'] = (loB - SCREEN_UPPER_LIMIT + SCREEN_HEIGHT - 3 + diff) % SCREEN_HEIGHT + SCREEN_UPPER_LIMIT
+            elem['lowerBound'] = (loB - SCREEN_UPPER_LIMIT + SCREEN_HEIGHT - (delta+1) + diff) % SCREEN_HEIGHT + SCREEN_UPPER_LIMIT
             
             asteroid_destroyed = False
             break
@@ -94,10 +98,11 @@ class Asteroids:
           if np.array_equal(obs[i][j], color):
 
             diff = j - elem['leftBound']
+            elem['spd'][0] = diff
             #print("h_diff =", diff)
 
             elem['leftBound'] = j
-            elem['rightBound'] = (riB - SCREEN_LEFT_LIMIT + SCREEN_WIDTH - 3 + diff) % SCREEN_WIDTH + SCREEN_LEFT_LIMIT
+            elem['rightBound'] = (riB - SCREEN_LEFT_LIMIT + SCREEN_WIDTH - (delta+1) + diff) % SCREEN_WIDTH + SCREEN_LEFT_LIMIT
 
             asteroid_destroyed = False
             break
@@ -111,12 +116,12 @@ class Asteroids:
         j += SCREEN_LEFT_LIMIT
       
       if asteroid_destroyed:
-        print ("DESTROYED asteroid(", ID, ") =", elem, "\n");
+        print("aaaaaa\n")
         destroyed_asteroids.append(ID)
-      else:
         print(ID, "-", elem, "\n")
     print()
 
     for i in destroyed_asteroids:
+      print("DESTROYED asteroid(", i, ") -", self.pos[i], "\n")
       del self.pos[i]
-
+      destroyed_asteroids.remove(i)
--- gym-retro/examples/myAI.py
+++ gym-retro/examples/myAI.py
@@ -5,6 +5,7 @@ import retro
 import time
 import numpy as np
 import queue
+import sys
 
 import posMethods as pos
 import speedMethods as spd
@@ -22,6 +23,7 @@ parser.add_argument('--scenario', '-s', default='scenario')
 parser.add_argument('--record', '-r', action='store_true')
 parser.add_argument('--verbose', '-v', action='count', default=1)
 parser.add_argument('--quiet', '-q', action='count', default=0)
+parser.add_argument("delta", type=int, default=4)
 
 args = parser.parse_args()
 env = retro.make('Asteroids-Atari2600', args.state or retro.STATE_DEFAULT, scenario=args.scenario, record=args.record)
@@ -30,6 +32,10 @@ verbosity = args.verbose - args.quiet
 ########################################################
 
 if __name__ == "__main__":
+  if (args.delta % 2 != 0): # se for ímpar
+    raise ValueError("Delta deve ser um número par maior ou igual a 4 (quatro)\nUsage: python3 myAI.py <delta>")
+
+  delta = args.delta
   obs = env.reset()
   t = 0
   asteroids = Asteroids.Asteroids(obs)
@@ -40,7 +46,10 @@ if __name__ == "__main__":
   print("===============")
 
   totrew = 0 # total reward
-  
+
+  # Vide Log (15/Jul/2018) para entender esta flag
+  got_reward = False
+
   while True:
     action = env.action_space.sample()
     obs, rew, done, info = env.step(action)
@@ -57,26 +66,34 @@ if __name__ == "__main__":
 #        print("hSPD =", ast[1])
 #        print("vSPD =", ast[2])
 
-    if t % 2 == 0:
-      print("t =", t, "\n")
+    if t % args.delta == 0:
+      print("t =", t, "| Delta =", delta, "\n")
+      if not got_reward:
+        print("findInitialObjects(obs):")
+        for elem in pos.findInitialObjects(obs):
+          print(elem)
 
-      print("findInitialObjects(obs):")
-      for elem in pos.findInitialObjects(obs):
-        print(elem)
-
-      print("\nAsteroids.update_pos(obs):")
-      asteroids.update_pos(obs)
-      #for k, elem in asteroids.get_asteroids().items():
-        #print(asteroids.get_asteroids()[elem])
-        #print(k, "-", elem)
-      #print()
-      print("==============")
-      #time.sleep(3)
+        print("\nAsteroids.update_pos(obs):")
+        asteroids.update_pos(obs, delta)
+        if delta == 4:
+          delta = 2
+        #for k, elem in asteroids.get_asteroids().items():
+          #print(asteroids.get_asteroids()[elem])
+        # print(k, "-", elem)
+        #print()
+        print("==============")
+        time.sleep(1.5)
+      else:
+        print("got_reward = True!! Setando para False...\n")
+        got_reward = False
+        delta = 4
 
     if rew > 0:
       print("time =", t, "\nReward:", rew, "\n")
+      got_reward = True
     elif rew < 0:
       print("time =", t, "\nPenalty:", rew, "\n")
+      got_reward = True
 
     if done:
       env.render()
