60ee5fb9d659ca8531315ce03c6617dd352320ec
vitorkei <vitor_kei9@hotmail.com>
2018-07-17 19:53:29 -0300

    Função que encontra todos os asteróides da tela por BFS atualizada. A busca agora faz loops na tela, ou seja, se, por exemplo, um asteróide tiver uma metade na parte de cima da tela e a outra metade na parte debaixo porque está dando a volta, ele será visto como o mesmo objeto. Arquivo printSamply.py adicionado (serviu para ver como a matriz visited é manipulada)

--- gym-retro/examples/posMethods.py
+++ gym-retro/examples/posMethods.py
@@ -7,6 +7,9 @@ SCREEN_LOWER_LIMIT = 194
 SCREEN_LEFT_LIMIT  = 8
 SCREEN_RIGHT_LIMIT = 159
 
+SCREEN_HEIGHT = 177
+SCREEN_WIDTH = 152
+
 # Entrada:
 #    obs: observação do ambiente (matriz)
 #    visited: define pixels já visitados (matriz)
@@ -21,6 +24,7 @@ def asteroidBFS(obs, visited, i, j):
   q = queue.Queue()
 
   color = obs[i][j].copy()
+  #print("\n\n***********\nCOLOR =", color, " | (i, j) = (", i, ",", j, ")")
   visited[i][j] = True
   q.put((i, j))
 
@@ -32,35 +36,70 @@ def asteroidBFS(obs, visited, i, j):
   while q.qsize() > 0:
     m, n = q.get()
 
-    if not visited[m+1][n]: # pixel abaixo
-      if np.array_equal(obs[m+1][n], color):
-        visited[m+1][n] = True # não olha p/ mesmo pixel preto de novo
-        q.put((m+1, n))
-        if m+1 > lowerBound:
-          lowerBound += 1
+    # upB, loB, leB, riB ajudarão a identificar os novos limites do asteróide
+    m_up = m - SCREEN_UPPER_LIMIT
+    m_up += SCREEN_HEIGHT - 1
+    upB = m_up
+    m_up = m_up % SCREEN_HEIGHT
+    m_up += SCREEN_UPPER_LIMIT
+    
+    m_down = m - SCREEN_UPPER_LIMIT
+    m_down += SCREEN_HEIGHT + 1
+    loB = m_down
+    m_down = m_down % SCREEN_HEIGHT
+    m_down += SCREEN_UPPER_LIMIT
 
-    if not visited[m][n-1]: # pixel a esquerda
-      if np.array_equal(obs[m][n-1], color):
-        visited[m][n-1] = True
-        q.put((m, n-1))
-        if n-1 < leftBound:
-          leftBound -= 1
+    n_left = n - SCREEN_LEFT_LIMIT
+    n_left += SCREEN_WIDTH - 1
+    leB = n_left
+    n_left = n_left % SCREEN_WIDTH
+    n_left += SCREEN_LEFT_LIMIT
 
-    if not visited[m-1][n]: # pixel acima
-      if np.array_equal(obs[m-1][n], color):
-        visited[m-1][n] = True
-        q.put((m-1, n))
-        if m-1 < upperBound:
-          upperBound -= 1
+    n_right = n - SCREEN_LEFT_LIMIT
+    n_right += SCREEN_WIDTH + 1
+    riB = n_right
+    n_right = n_right % SCREEN_WIDTH
+    n_right += SCREEN_LEFT_LIMIT
+
+    if not visited[m_down][n]: # pixel abaixo
+      if np.array_equal(obs[m_down][n], color):
+        visited[m_down][n] = True
+        q.put((m_down, n))
+        if loB > lowerBound + SCREEN_HEIGHT - SCREEN_UPPER_LIMIT:
+          #print("lowerBound | loB | m_down =", lowerBound, "|", loB, "|", m_down) 
+          lowerBound = m_down
+
+    if not visited[m][n_left]: # pixel a esquerda
+      if np.array_equal(obs[m][n_left], color):
+        visited[m][n_left] = True
+        q.put((m, n_left))
+        if leB < leftBound + SCREEN_WIDTH - SCREEN_LEFT_LIMIT:
+          #print("leftBound | leB | n_left =", leftBound, "|", leB, "|", n_left)
+          leftBound = n_left
+
+    if not visited[m_up][n]: # pixel acima
+      if np.array_equal(obs[m_up][n], color):
+        visited[m_up][n] = True
+        q.put((m_up, n))
+        if upB < upperBound + SCREEN_HEIGHT - SCREEN_UPPER_LIMIT:
+          #print("upperBound | upB | m_up =", upperBound, "|", upB, "|", m_up)
+          upperBound = m_up
     
     if n < SCREEN_RIGHT_LIMIT:
-      if not visited[m][n+1]: # pixel a direita
-        if np.array_equal(obs[m][n+1], color):
-          visited[m][n+1] = True
-          q.put((m, n+1))
-          if n+1 > rightBound:
-            rightBound += 1
+      if not visited[m][n_right]: # pixel a direita
+        if np.array_equal(obs[m][n_right], color):
+          visited[m][n_right] = True
+          q.put((m, n_right))
+          if riB > rightBound + SCREEN_WIDTH - SCREEN_LEFT_LIMIT:
+            #print("rightBound | riB | n_right =", rightBound, "|", riB, "|", n_right)
+            rightBound = n_right
   
+  #for m in range (210):
+    #for n in range(160):
+      #if visited[m][n]:
+        #print(m, n)
+  #input("esperando...")
+
   return {'color': color,
           'upperBound': upperBound,
           'lowerBound': lowerBound,
@@ -82,8 +121,8 @@ def findInitialObjects(obs):
 
   objsPos = []
 
-  for i in range(18, 195):
-    for j in range(8, 159):
+  for i in range(SCREEN_UPPER_LIMIT, SCREEN_LOWER_LIMIT+1):
+    for j in range(SCREEN_LEFT_LIMIT, SCREEN_RIGHT_LIMIT+1):
       if not visited[i][j]:
         if sum(obs[i][j]) > 0:
           objsPos.append(asteroidBFS(obs, visited, i, j))
